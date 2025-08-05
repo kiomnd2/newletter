ed18e8ca35d53595804d2754c6ab5015dc97a34b
kiomnd2 <kiomnd2@gmail.com>
2025-08-05 22:57:03 +0900

    wip: 도메인 모델 작성

---
+++ src/test/java/kr/kiomn2/newsletter/newsletter/NewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kiomn2.newsletter.newsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
